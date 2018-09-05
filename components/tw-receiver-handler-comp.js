16adc1ef2f0d7849551848d01970bcaf22ffb071
sendwheel <derrickdool@gmail.com>
2018-09-05 12:14:50 -0400

    Initial commit

---
+++ components/tw-receiver-handler-comp.js
@@ -0,0 +1,173 @@
+/*\
+title: $:/plugins/sendwheel/tw-receiver/tw-receiver-handler.js
+type: application/javascript
+module-type: saver
+
+Handles saving wiki via POST to server storage
+
+\*/
+
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+Create a saver module
+*/
+var ReceiverSaver = function(wiki) {
+};
+
+ReceiverSaver.prototype.save = function(text,method,callback) {
+	
+	// if this module is not enabled by the user return false so that saver-handler can move on to next option
+	// we do this here, instead of in canSave(), so that the saver can be enabled/disabled without a page refresh
+	if(($tw.wiki.getTextReference("$:/tw-receiver-enabled") || "").toLowerCase() !== "yes") {
+		return false;
+	}
+	
+	// retrieve parameters from the ui form
+	//var isEnabled = $tw.wiki.getTextReference("$:/tw-receiver-enabled");
+	var seckey = $tw.utils.getPassword("tw-receiver-seckey"); //reminder: this util uses html5.localStorage (secvio)
+	var wikiname = $tw.wiki.getTextReference("$:/tw-receiver-wikiname");
+	var serverurl = $tw.wiki.getTextReference("$:/tw-receiver-serverurl");
+	var cdauthentication = true;
+	var signdata = true;
+	
+	// if we're not provided a filename, try to get the name of the wiki from the URL
+	if(!wikiname) {
+		var p = document.location.pathname.lastIndexOf("/");
+		if(p !== -1) {
+			// We decode the pathname because document.location is URL encoded by the browser
+			wikiname = decodeURIComponent(document.location.pathname.substr(p+1));
+		}
+	}
+	
+	if(!wikiname) {
+		wikiname = "tiddlywiki.html";
+	}
+	
+	// construct the server url if not provided
+	if(!serverurl) {
+		serverurl = "tw-receiver-server.php";
+	}
+	
+	// fail to save if we're missing any critical parameters
+	if(
+		!seckey || seckey.toString().trim() === "" ||
+		!wikiname || wikiname.toString().trim() === "" ||
+		!serverurl || serverurl.toString().trim() === "" 
+	){
+		return callback("TW Receiver: \n Missing some user input parameters. Save Failed");
+		// we don't return false because we want the user to fix the problem or disable this module
+		//return false;
+	}
+	
+	// everything seems in order, lets attempt the save
+	// display the save starting notification
+	$tw.notifier.display("$:/language/Notifications/Save/Starting");
+	
+	// helper function returns a sha256 hex digest using the sjcl lib
+	var getSHA256 = function(data) {
+		var sjcl = $tw.node ? (global.sjcl || require("./sjcl.js")) : window.sjcl;
+		return sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(data))
+	}
+	
+	// helper function encapsulates the actual data post
+	// modified version of the upload.js saver from TiddlyWiki core
+	var postToServer = function(){
+		// text,seckey,filename,serverurl
+		var retResp = false;
+		// assemble the header
+		var boundary = "-------" + "81fd830c85363675edb98d2879916d8c";	
+		var header = [];
+		header.push("--" + boundary + "\r\nContent-disposition: form-data; name=\"twreceiverparams\"\r\n");
+		header.push("seckey=" + seckey + "&wikiname=" + wikiname + "&datasig=" + datasig); 
+		header.push("\r\n" + "--" + boundary);
+		header.push("Content-disposition: form-data; name=\"userfile\"; filename=\"" + wikiname + "\"");
+		header.push("Content-Type: text/html;charset=UTF-8");
+		header.push("Content-Length: " + text.length + "\r\n");
+		header.push("");
+		// assemble the tail and the data itself
+		var tail = "\r\n--" + boundary + "--\r\n";
+		var	data = header.join("\r\n") + text + tail;
+		// do the HTTP post
+		var http = new XMLHttpRequest();
+		http.open("POST",serverurl,true);
+		http.setRequestHeader("Content-Type","multipart/form-data; charset=UTF-8; boundary=" + boundary);
+		http.onreadystatechange = function() {
+			if(http.readyState == 4 && http.status == 200) {
+				if(http.responseText.substr(0,8) === "000 - ok") {
+					callback(null);
+				} else {
+					callback("Error:\n" + http.responseText);
+				}
+			}
+		};
+		try {
+			http.send(data);
+		} catch(ex) {
+			return callback($tw.language.getString("Error/Caption") + ":" + ex);
+		}
+	};
+	
+	// if signdata is enabled, create a data integrity signature
+	var datasig = "";
+	if(signdata){
+		datasig = getSHA256(text+seckey);
+	}
+	
+	// cdauthentication mode check
+	if(cdauthentication) {
+		var xhrequest = new XMLHttpRequest();
+		xhrequest.onreadystatechange = function() {
+			if(xhrequest.readyState == 4 && xhrequest.status == 200) {
+				var challengetoken = xhrequest.responseText;
+				seckey = getSHA256(seckey + challengetoken);
+				// post helper
+				postToServer();
+			}
+		};
+		xhrequest.open("GET", serverurl + "?md=gct", true);
+		try {
+			xhrequest.send(null);
+		} catch(ex) {
+			return callback($tw.language.getString("Error/Caption") + ":" + ex);
+		}
+	}
+	else {
+		// post helper
+		postToServer();
+	}
+	
+	// we return true because the attempt was completed
+	// either saved success or error reported via callback()
+	return true;
+};
+
+/*
+Information about this saver
+*/
+ReceiverSaver.prototype.info = {
+	name: "tw-receiver",
+	priority: 3000, // priority: higher # is first
+	capabilities: ["save", "autosave"]
+};
+
+/*
+Static method that returns true if this saver is capable of working
+Called onload (wiki start) to enable this saver, requires refresh to change
+*/
+exports.canSave = function(wiki) {
+	return true;
+};
+
+/*
+Create an instance of this saver
+*/
+exports.create = function(wiki) {
+	return new ReceiverSaver(wiki);
+};
+
+})();
---
+++ tw-receiver-server.php
@@ -0,0 +1,327 @@
+<?php
+// during initial setup set $debug = true
+// accessing tw-receiver-server.php directly (GET Request) will perform some access tests and report
+// if this is off, most error reporting is disabled throughout
+$debug = true;
+
+// the backup mechanism is very basic, and only keeps x last saves
+// backup directory, default is fine. set $backupdir=false to disable backups
+$backupdir = 'twbackups';
+// number of backups to keep 
+$backupcount = 10;
+
+// enable the use of an ini file to store secret key outside of the web accessible directory (good practice)
+// requires placing the twreceiver_config.ini in an external directory and providing the path
+// Example: $extSecKeyPath = "../privatedir/tw-receiver-config.ini"
+// if you don't have access to a non accessible space, or aren't sure how to do this, do not use this feature
+// instead set $extSecKeyPath = false and use the $password variable
+// wherever you set the password, have fun with it and make it a long list of words
+$extSecKeyPath = false; 
+$userpassword = "hello i'm a short friendly password"; 
+
+// there's no way to securely transmit over HTTP. Using HTTP your password and content can be viewed and changed
+// use of HTTPS (TLS) is strongly recommended.
+// the following setting enables Digest Authentication and Server Challenge Request Mode. 
+// Think of it as low budget security. This will prevent a number of attacks, but it is not a replacement for proper HTTPS
+// Try out HTTPS, check out https://letsencrypt.org/
+$challengeDigestAuthentication = true;
+
+// enable data integrity signing (recommended)
+// this creates a unique signature of the wiki text and the secret key being passed in
+// checking the validity of this signature helps to prevent tampering of the payload mid stream
+$dataIntegritySigning = true;
+
+
+function processPostParams($poststring, $delim_major = '&', $delim_minor = '='){
+	$return_arr = [];
+	
+	foreach (explode($delim_major,$poststring) as $value) {
+		$minorsplit = explode($delim_minor, $value);
+		$return_arr[$minorsplit[0]] = $minorsplit[1];
+	}
+	
+	return $return_arr;
+}
+
+function failWithMsg($msg){
+	global $debug;
+	
+	if(!$debug) {
+		exit('debug is disabled');
+	}
+	
+    exit($msg);
+}
+
+function performBackup($currentfile){
+	global $backupdir, $backupcount;
+	
+	// check that backups are enabled
+	if(!$backupdir){
+		return false;
+	}
+	
+	// verify file exists
+	if(!file_exists($currentfile)) {
+		return false;
+	}
+	
+	//verify backup dir exists
+	if(!is_dir($backupdir)) {
+		if(!mkdir($backupdir, 0755)){
+			failWithMsg('Server Error: unable to create backup directory');
+		}
+	}
+	
+	//move existing file and timestamp it
+	$filenameparts = pathinfo($currentfile);
+	if($filenameparts['extension'] != 'html' &&  $filenameparts['extension'] != 'htm'){
+		failWithMsg('Server Error: bad file extension');
+	}
+	
+	$backupfile = $backupdir . '/' . $filenameparts['filename'] . '.' . time() . '.' . $filenameparts['extension'];
+	if(!rename($currentfile, $backupfile)) {
+		failWithMsg('Server Error: backup failed, could not move file');
+	}
+	
+	// perform backup pruning duties
+	$dircontents = array_diff(scandir($backupdir, 1), array('..', '.')); //removes . and .. dir listings
+	// remove dirs from array
+	foreach ($dircontents as $key => $val) {
+		if(is_dir($backupdir.'/'.$val)){
+			unset($dircontents[$key]);
+		}
+	}
+
+	if(count($dircontents) < $backupcount){
+		return true; //no prune required
+	}
+	
+	// really basic pruning mechanism, could be improved with date logic
+	foreach($dircontents as $k => $file){
+		if($k > $backupcount-1) {
+			if(!unlink($backupdir.'/'.$file)){
+				failWithMsg('Server Error: backup pruning failure, could not remove file');
+			}
+		}
+	}
+	
+	return true; // complete backup success
+}
+
+function authenticateRequest($submittedkey){
+	global $challengeDigestAuthentication;
+	$sharedkey= getSharedSecret();
+	
+	// check for challengeDigestAuthentication mode
+	if(!$challengeDigestAuthentication){
+		if($sharedkey === $submittedkey) {
+			return true;
+		}
+	}
+	elseif($challengeDigestAuthentication) {
+		if(testChallengeKey($submittedkey, $sharedkey)){
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+function createChallengeToken(){
+
+	// start a session for this request chain
+	session_start();
+	$ctoken = hash("sha256", time()+random_int(100,1999));
+	//$ctoken = hash("sha256", $iterator . gmdate('YmdHi')); //time based expiry
+	// store challenge token to session
+	$_SESSION["ctoken"]= $ctoken;
+	
+	return $ctoken;
+}
+
+function getSharedSecret(){
+	global $extSecKeyPath, $userpassword;
+	$sharedkey="";
+	if($extSecKeyPath){
+		$config = parse_ini_file($extSecKeyPath);
+		$sharedkey = $config['seckey'];
+	}
+	else {
+		$sharedkey = $userpassword;
+	}
+	
+	return $sharedkey;
+}
+
+function testChallengeKey($submittedkey, $sharedsecret){
+	session_start();
+	$ckey = hash("sha256", $sharedsecret . $_SESSION["ctoken"]);
+	session_destroy();
+
+	if($ckey === trim($submittedkey)){
+		return true;
+	}
+	return false;
+}
+
+function checkDataSignature($submitteddatasig, $datafile) {
+	$seckey = getSharedSecret();
+	$textdata = file_get_contents($datafile);
+	$datasig = hash("sha256", $textdata . $seckey);
+	
+	if(trim($submitteddatasig) == $datasig) {
+		return true;
+	}
+	
+	return false;
+}
+/*
+function testChallengeKey($submittedkey, $sharedsecret){
+	$ckey = hash("sha256", $sharedsecret . hash("sha256", getIterator() . gmdate('YmdHi'))); //valid now
+	$ckey2 = hash("sha256", $sharedsecret . hash("sha256", getIterator() . gmdate('YmdHi', (time()-60)))); //valid 1 min ago
+
+	if($ckey === $submittedkey || $ckey2 === $submittedkey){
+		return true;
+	}
+	return false;
+}
+*/
+
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+	
+	// logic to return challenge token 
+	if(isset($_GET['md']) && $_GET['md'] == 'gct'){
+		if($challengeDigestAuthentication){
+			echo createChallengeToken();
+			exit();
+		}
+	}
+	
+	// if debug mode, show setup test page
+	if(!$debug) {
+		exit('debug is disabled');
+	}
+?>
+<h1>Debug Tests</h1>
+<table>
+<tr>
+	<td>ini setting: file_uploads</td>
+	<td><?php echo (ini_get('file_uploads')==1)?'OK':'<span style="color:red">FAIL</span>' ?></td>
+</tr>
+<tr>
+	<td>ini setting: upload_max_filesize</td>
+	<td><?php echo ini_get('upload_max_filesize') ?></td>
+</tr>
+<tr>
+	<td>ini setting: post_max_size</td>
+	<td><?php echo ini_get('post_max_size') ?></td>
+</tr>
+<tr>
+	<td>backups enabled</td>
+	<td><?php echo ($backupdir)?'YES':'NO' ?></td>
+</tr>
+<tr>
+	<td>backups max count</td>
+	<td><?php echo $backupcount ?></td>
+</tr>
+<tr>
+	<td>backups directory exists</td>
+	<td><?php echo (is_dir($backupdir))?'OK':'<span style="color:red">FAIL</span>' ?></td>
+</tr>
+<tr>
+	<td>backups directory is writable</td>
+	<td><?php echo (is_writable($backupdir))?'OK':'<span style="color:red">FAIL</span>' ?></td>
+</tr>
+<tr>
+	<td>this directory is writable</td>
+	<td><?php echo (is_writable("."))?'OK':'<span style="color:red">FAIL</span>' ?></td>
+</tr>
+<tr>
+	<td>external key enabled</td>
+	<td><?php echo ($extSecKeyPath)?'YES':'NO' ?></td>
+</tr>
+<tr>
+	<td>external key reachable</td>
+	<td><?php echo (file_exists($extSecKeyPath))?'YES':'NO' ?></td>
+</tr>
+<tr>
+	<td>secure connection (https)</td>
+	<td><?php echo (@$_SERVER['HTTPS'] != "")?'YES':'NO' ?></td>
+</tr>
+<tr>
+	<td>challenge digest auth mode</td>
+	<td><?php echo ($challengeDigestAuthentication)?'YES':'NO' ?></td>
+</tr>
+<tr>
+	<td>check data integrity signature</td>
+	<td><?php echo ($dataIntegritySigning)?'YES':'NO' ?></td>
+</tr>
+</table>
+
+<br /><b>Notes:</b>
+<br />- Your <i>upload_max_filesize</i> and <i>post_max_size</i> must be at least larger than your wiki filesize
+<br />- On NGINX client_max_body_size is another parameter worth looking at if uploads fail with <i>413 Request Entity Too Large</i>
+<?php
+	exit();
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+	if(isset($_POST['twreceiverparams'])){
+		$postparameters = processPostParams($_POST['twreceiverparams']);
+		
+		if(isset($postparameters['seckey']) && $postparameters['seckey'] != ""){
+			$submittedkey = $postparameters['seckey'];
+		}
+		else {
+			failWithMsg('Server Error: Missing Authentication Parameters');
+		}
+	}
+	else {
+		failWithMsg('Server Error: Missing Parameters');
+	}
+
+	// check credentials
+	if(!authenticateRequest($submittedkey)){
+		failWithMsg('Server Error: Authentication Failure');
+		exit();
+	}
+	
+	// check data integrity
+	if($dataIntegritySigning) {
+		if(isset($postparameters['datasig']) && $postparameters['datasig'] != ""){
+			$submitteddatasig = $postparameters['datasig'];
+			if(!checkDataSignature($submitteddatasig, $_FILES['userfile']['tmp_name'])){
+				failWithMsg('Server Error: Data Integrity Failure');
+			}
+		}
+		else {
+			failWithMsg('Server Error: Missing Data Signature');
+		}
+	}
+
+	$destinationfile = "./" . basename($_FILES['userfile']['name']);
+	$file_extension = strtolower(pathinfo($destinationfile,PATHINFO_EXTENSION));
+
+	// not interested in file types other than html
+	if($file_extension != "html" && $file_extension != "htm") {
+		failWithMsg('Server Error: bad file extension');
+	}
+	
+	// if file already exists, backup
+	if(file_exists($destinationfile)) {
+		if(!performBackup($destinationfile)  && $backupdir){
+			failWithMsg('Server Error: backup failure');
+		} 
+	}
+
+	// this method ensures we're moving and renaming an uploaded file
+	if(move_uploaded_file($_FILES['userfile']['tmp_name'], $destinationfile)) {
+		echo "000 - ok";
+	}
+	else {
+		failWithMsg('Upload Failed');
+	}
+}
+?>
